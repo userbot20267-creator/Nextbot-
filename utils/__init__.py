b8c4832b2d53f008b974edaaf3f0507aba566c1a
userbot20267-creator <userbot20267@gmail.com>
2026-04-15 17:47:27 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,5 @@
+# utils/__init__.py
 
+from .check_sub import check_user_subscription, get_required_channels_from_db
+from .scraping import search_external_books, fetch_book_details
+from .broadcast import broadcast_message
