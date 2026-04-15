29bb7e2aef4f6698086bbbe9e796692ab8dd74cc
userbot20267-creator <userbot20267@gmail.com>
2026-04-15 18:19:14 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,64 @@
+# models/__init__.py
 
+from .schemas import (
+    # نماذج المستخدم
+    UserSchema,
+    UserStatsSchema,
+    
+    # نماذج الأقسام
+    CategorySchema,
+    CategoryStatsSchema,
+    
+    # نماذج المؤلفين
+    AuthorSchema,
+    
+    # نماذج الكتب
+    BookSchema,
+    BookStatsSchema,
+    BookDetailSchema,
+    
+    # نماذج الإعدادات
+    SettingSchema,
+    RequiredChannelSchema,
+    
+    # نماذج البحث
+    SearchQuerySchema,
+    ExternalBookResultSchema,
+    SearchResultsSchema,
+    
+    # نماذج الإذاعة
+    BroadcastMessageSchema,
+    BroadcastResultSchema,
+    
+    # نماذج الحظر
+    BanUserSchema,
+    
+    # نماذج التغذية الراجعة
+    FeedbackSchema,
+    
+    # نماذج الردود
+    SuccessResponseSchema,
+    ErrorResponseSchema,
+)
+
+__all__ = [
+    "UserSchema",
+    "UserStatsSchema",
+    "CategorySchema",
+    "CategoryStatsSchema",
+    "AuthorSchema",
+    "BookSchema",
+    "BookStatsSchema",
+    "BookDetailSchema",
+    "SettingSchema",
+    "RequiredChannelSchema",
+    "SearchQuerySchema",
+    "ExternalBookResultSchema",
+    "SearchResultsSchema",
+    "BroadcastMessageSchema",
+    "BroadcastResultSchema",
+    "BanUserSchema",
+    "FeedbackSchema",
+    "SuccessResponseSchema",
+    "ErrorResponseSchema",
+]
