efba2a6a9072f19e0c45a8461e97cf4a053cbcae
userbot20267-creator <userbot20267@gmail.com>
2026-04-15 18:20:15 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,27 @@
+# services/__init__.py
 
+from .scraper import (
+    search_open_library,
+    search_google_books,
+    search_external_books,
+    fetch_book_details,
+)
+
+from .broadcaster import (
+    broadcast_message,
+    broadcast_to_users,
+    get_broadcast_stats,
+)
+
+__all__ = [
+    # Scraper
+    "search_open_library",
+    "search_google_books",
+    "search_external_books",
+    "fetch_book_details",
+    
+    # Broadcaster
+    "broadcast_message",
+    "broadcast_to_users",
+    "get_broadcast_stats",
+]
