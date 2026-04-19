ae93cf57b676b5c91dd75dcaa17b1a736b987e65
userbot20267-creator <userbot20267@gmail.com>
2026-04-19 22:13:54 +0300

    Update scheduler.py

--- features/reminders/scheduler.py
+++ features/reminders/scheduler.py
@@ -1,5 +1,4 @@
 import datetime
-from apscheduler.triggers.cron import CronTrigger
 from .reporter import send_reminders
 
 async def reminder_callback(context):
@@ -8,9 +7,9 @@ async def reminder_callback(context):
 def schedule_reminders(application):
     job_queue = application.job_queue
     if job_queue:
-        # تشغيل كل يومين عند الساعة 10 صباحاً (الاثنين، الأربعاء، الجمعة، الأحد)
-        job_queue.run_custom(
+        job_queue.run_daily(
             reminder_callback,
-            trigger=CronTrigger(day_of_week='mon,wed,fri,sun', hour=10, minute=0, timezone=datetime.timezone.utc),
+            time=datetime.time(hour=10, minute=0, tzinfo=datetime.timezone.utc),
+            days=(0, 2, 4, 6),
             name="incomplete_books_reminder"
         )
