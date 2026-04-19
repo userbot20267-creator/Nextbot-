e4ed65cad225ef86e1653b0099c179e2ff05e0a6
userbot20267-creator <userbot20267@gmail.com>
2026-04-19 22:13:17 +0300

    Update scheduler.py

--- features/weekly_report/scheduler.py
+++ features/weekly_report/scheduler.py
@@ -1,19 +1,16 @@
 # features/weekly_report/scheduler.py
 import datetime
-from apscheduler.triggers.cron import CronTrigger
 from .reporter import send_weekly_report
 
 async def weekly_report_callback(context):
-    """الدالة التي ستستدعى أسبوعياً"""
     await send_weekly_report(context.bot)
 
 def schedule_weekly_report(application):
-    """جدولة التقرير الأسبوعي (كل يوم أحد الساعة 9 صباحاً)"""
     job_queue = application.job_queue
     if job_queue:
-        # تشغيل كل يوم أحد الساعة 9:00 UTC
-        job_queue.run_custom(
+        job_queue.run_daily(
             weekly_report_callback,
-            trigger=CronTrigger(day_of_week='sun', hour=9, minute=0, timezone=datetime.timezone.utc),
+            time=datetime.time(hour=9, minute=0, tzinfo=datetime.timezone.utc),
+            days=(6,),
             name="weekly_report"
         )
