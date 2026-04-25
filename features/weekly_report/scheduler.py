790774f48f3292b3350ae89a2f2a9d0a7dd4647c
userbot20267-creator <userbot20267@gmail.com>
2026-04-25 23:19:17 +0300

    Update scheduler.py

--- features/weekly_report/scheduler.py
+++ features/weekly_report/scheduler.py
@@ -1,4 +1,3 @@
-# features/weekly_report/scheduler.py
 import datetime
 from .reporter import send_weekly_report
 
@@ -11,6 +10,6 @@ def schedule_weekly_report(application):
         job_queue.run_daily(
             weekly_report_callback,
             time=datetime.time(hour=9, minute=0, tzinfo=datetime.timezone.utc),
-            days_of_week=(6,),  # تم التعديل: days -> days_of_week
+            days=(6,),
             name="weekly_report"
         )
